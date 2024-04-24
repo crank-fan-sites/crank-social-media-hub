7cb535dc62b62e0878139d44aedbe7000503dd3e
Saadh S <atextbooksituation@gmail.com>
2024-04-24 16:49:32 -0500

    Discord working

--- components/front_page/row-discord.tsx
+++ components/front_page/row-discord.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 
 import Discord from "@/components/front_page/discord";
@@ -7,11 +8,37 @@ import { HeadingH3, Paragraph } from "@/components/typography";
 import Image from "next/image";
 
 const RowDiscord: NextPage = () => {
+  const [info, setInfo] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/discord/content");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const thedata = await response.json();
+        setInfo(thedata);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div className="grid grid-cols-1 border-t md:grid-cols-5 bg-primary border-stone-400 dark:border-stone-600">
       <div className="col-span-5 px-2 py-6 border-b lg:col-span-3 group md:p-8 lg:p-12 border-stone-400 dark:border-stone-600 md:border-b-0 hover:bg-scanlines">
         <HeadingH3 className="text-base group-hover:text-background group-hover:font-bold">
-          Recent Discord General Chat Messages
+          Recent Discord {info.name} Chat Messages
         </HeadingH3>
         <div className="grid grid-cols-1 border-t bg-primary border-stone-400 dark:border-stone-600">
           <Discord />
@@ -24,7 +51,7 @@ const RowDiscord: NextPage = () => {
 
         <div className="patreon-container">
           <iframe
-            src="https://discord.com/widget?id=855475575217061898&theme=dark"
+            src={info.widget}
             width="350"
             height="500"
             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
