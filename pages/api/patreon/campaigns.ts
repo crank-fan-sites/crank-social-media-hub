2f6cac4081e81cf0074553e24583c3c7e2a8465a
Saadh S <atextbooksituation@gmail.com>
2024-04-27 05:04:23 -0400

    Patreon api gets the first campaign ID on its own

--- pages/api/patreon/campaigns.ts
+++ pages/api/patreon/campaigns.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import getStrapi from "@/lib/getStrapi";
 
-async function fetchCampaignId(accessToken) {
+export default async function handler(req, res) {
+  const accessToken = (await getStrapi("/social-media-patreon")).access_token;
   const url = `https://www.patreon.com/api/oauth2/api/current_user/campaigns?include=campaigns&fields${encodeURIComponent(
     "["
   )}post${encodeURIComponent("]")}=id`;
@@ -11,32 +13,11 @@ async function fetchCampaignId(accessToken) {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    // const campaigns = response.data.data;
+    const campaigns = response.data.data;
     const campaignId = response.data.data[0].id; // Assuming the user has at least one campaign
-    return campaignId;
-  } catch (error) {
-    console.error("Error fetching campaign ID:", error.message);
-    throw error;
-  }
-}
-
-export default async function handler(req, res) {
-  const accessToken = process.env.NEXT_PUBLIC_PATREON_ACCESS_TOKEN;
-  const campaignId = await fetchCampaignId(accessToken);
-  const url = `https://www.patreon.com/api/oauth2/v2/campaigns/${campaignId}/posts?fields${encodeURIComponent(
-    "["
-  )}post${encodeURIComponent(
-    "]"
-  )}=title,content,is_paid,is_public,published_at,url,embed_data,embed_url,app_id,app_status`;
-
-  try {
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    res.status(200).json({ status: response.status, ...response.data });
+    res
+      .status(200)
+      .json({ status: response.status, campaigns, id: campaignId });
   } catch (error) {
     if (error.response) {
       res.status(error.response.status).json({
