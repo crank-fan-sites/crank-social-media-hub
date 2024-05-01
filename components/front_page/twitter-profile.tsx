2a7e5dcde62ff4348d4a339b426326ff9f600577
Saadh S <atextbooksituation@gmail.com>
2024-05-01 16:46:10 -0400

    Twitter Scrapi integration

--- components/front_page/twitter-profile.tsx
+++ components/front_page/twitter-profile.tsx
@@ -1,26 +1,49 @@
 import type { NextPage } from "next";
-
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 import { Timeline } from "react-twitter-widgets";
 
-import { Button } from "@/components/ui/button";
-import {
-  HeadingH1,
-  HeadingH2,
-  HeadingH3,
-  Paragraph,
-} from "@/components/typography";
-
 const TwitterProfle: NextPage = () => {
+  const [profile, setProfile] = useState(null);
+  const [darkMode, setDarkMode] = useState(false);
+  const [height, setHeight] = useState(800);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/twitter/content");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const thedata = await response.json();
+
+        setProfile(thedata.profile);
+        setDarkMode(thedata.darkMode);
+        setHeight(thedata.widgetHeight);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
   return (
-    <Timeline
-      dataSource={{
-        sourceType: "profile",
-        screenName: "UnelectableA",
-      }}
-      options={{ theme: "dark", height: 800 }}
-    />
+    profile && (
+      <Timeline
+        dataSource={{
+          sourceType: "profile",
+          screenName: profile,
+        }}
+        options={{ theme: darkMode ? "dark" : "light", height }}
+      />
+    )
   );
 };
 
---
+++ pages/api/twitter/content.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import getStrapi from "@/lib/getStrapi";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { profile, dark_mode, widget_height } = await getStrapi(
+      "/social-media-twitter"
+    );
+    const darkMode = dark_mode ?? false;
+    const widgetHeight = widget_height ?? 800;
+    res.status(200).json({ profile, darkMode, widgetHeight });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+}
